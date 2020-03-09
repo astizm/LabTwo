6a375f9b84412b4c3d81072475e4fd28ebd84ed6
astizm <asti-maera@live.nmit.ac.nz>
2020-03-10 12:47:15 +1300

    sum of cubes

---
+++ index.js
@@ -0,0 +1,14 @@
+//create variable to create cubes
+let a = 12**3
+let x = 6**3
+let b = 24**3
+let y = 18**3
+
+//show sum of cubes
+console.log(a);//1728
+console.log(b);//13824
+console.log(x);//216
+console.log(y);//5832
+console.log(a + y);//7560
+console.log(a + y, b + y );//7560, 19656
+console.log (a + b + x + y);//21600
